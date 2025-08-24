02ea21a17f9d04dafe77b1ff7e6c627a450534e5
rzjuliannofficial <nabhanrizqijuliansaputro@gmail.com>
2025-08-24 12:11:15 +0700

    Add Sesi 4

---
+++ Pyton/Sesi4/games/cuypy.py
@@ -0,0 +1,39 @@
+import random
+def start():
+    nama_user = input("Masukkan nama kamu: ")
+
+    while nama_user == "":
+        nama_user = input("Masukkan dulu nama kamu: ")
+
+    while True:
+        cuypy_position = random.randint(1, 4)
+        bentuk_goa = "|_|"
+        goa = [bentuk_goa] * 4
+        goa_kosong = goa.copy()
+        goa[cuypy_position - 1] = "|0_^|"
+        goa_kosong = '-'.join(goa_kosong)
+        goa = '-'.join(goa)
+        
+        print(f'Halo {nama_user}! Coba perhatikan goa dibawah ini \n\n{goa_kosong}\n')
+
+        pilihan_user = int(input("Menurut kamu di goa nomor berapa CUYPY berada? [1 / 2 / 3 / 4]: "))
+        #input itu string, jadi harus diubah ke int jika ingin dibandingkan dengan angka
+
+        print (f"pilihan kamu adalah {pilihan_user}")
+        print()
+
+        if pilihan_user == cuypy_position:
+            print(f"\nSelamat {nama_user}, kamu berhasil.")
+            print(f"\n{goa}")
+        else:
+            print(f"\nYahh {nama_user}... kamu salah nihh.")
+            print(f"\n{goa}")
+            
+        play_again = input("\nMau main lagi? [y/n]:")
+        if play_again.lower() == 'n':
+            print("Terima kasih sudah bermain. Sampai jumpa lagi!")
+            break
+
+
+if __name__ == "__main__":
+    start()
