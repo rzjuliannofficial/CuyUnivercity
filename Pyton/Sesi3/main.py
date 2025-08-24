cf7c05fa6a2483b211fc6b80989cfcba5d741804
rzjuliannofficial <nabhanrizqijuliansaputro@gmail.com>
2025-08-24 11:27:43 +0700

    update

--- Pyton/Sesi3/main.py
+++ Pyton/Sesi3/main.py
@@ -1,13 +1,11 @@
 #library
 import random
+from libs import welcome_message
 
-welcomeMessage = "CUYPY GAMES!" 
 cuypy_position = random.randint(1, 4)
 
-print("******************")
-print(f"** {welcomeMessage} **")
-print("******************")
-    
+welcome_message("WELCOME TO CUYPY GAME")
+
 #Sesi 3
 # for i in range(5).__reversed__():
 #     # print(f"** GOA {i} **")    
@@ -34,35 +32,44 @@ goa = '-'.join(goa)
 
 
 nama_user = input("Masukkan nama kamu: ")
+
 while nama_user == "":
     nama_user = input("Masukkan dulu nama kamu: ")
-    
-print(f'''
+
+while True:
+    print(f'''
 Halo {nama_user}! Coba perhatikan goa dibawah ini 
 {goa_kosong}
 ''')
 
-pilihan_user = int(input("Menurut kamu di goa nomor berapa CUYPY berada? [1 / 2 / 3 / 4]: "))
-#input itu string, jadi harus diubah ke int jika ingin dibandingkan dengan angka
+    pilihan_user = int(input("Menurut kamu di goa nomor berapa CUYPY berada? [1 / 2 / 3 / 4]: "))
+    #input itu string, jadi harus diubah ke int jika ingin dibandingkan dengan angka
 
-print (f"pilihan kamu adalah {pilihan_user}")
-print()
-konfirmasi_jawaban = input("Apa kamu yakin dengan pilihanmu?[y/n]: ")
-while konfirmasi_jawaban == "":
-    konfirmasi_jawaban = input("Kamu harus jawab dulu! [y/n]: ")
-    
-if konfirmasi_jawaban.lower() == 'n':
-    print("oh ga yakin yah? keluar sistem...")
-    exit()
-elif konfirmasi_jawaban.lower() == 'y':
-    if pilihan_user == cuypy_position:
-        print(f"Selamat {nama_user}, kamu berhasil. {goa}")
+    print (f"pilihan kamu adalah {pilihan_user}")
+    print()
+    konfirmasi_jawaban = input("Apa kamu yakin dengan pilihanmu?[y/n]: ")
+    while konfirmasi_jawaban == "":
+        konfirmasi_jawaban = input("Kamu harus jawab dulu! [y/n]: ")
+        
+    if konfirmasi_jawaban.lower() == 'n':
+        print("oh ga yakin yah? keluar sistem...")
+        exit()
+    elif konfirmasi_jawaban.lower() == 'y':
+        if pilihan_user == cuypy_position:
+            print(f"\nSelamat {nama_user}, kamu berhasil.")
+        else:
+            print(f"\nYahh {nama_user}... kamu salah nihh.")
+            surrend = input("nyerah? [y/n]: ")
+            if surrend.lower() == 'y':
+                print(f"CUYPY ada di goa nomor {cuypy_position}")
+                print(f"\n{goa}")
     else:
-        print(f"Yahh {nama_user}... kamu salah nihh. {goa}")
-else:
-    print("Pilihan tidak valid. Program akan keluar.")
-    exit()
-    
-    
+        print("Pilihan tidak valid. Program akan keluar.")
+        exit()
+        
+    play_again = input("Mau main lagi? [y/n]:")
+    if play_again.lower() == 'n':
+        break
 
+print("Terima kasih sudah bermain. Sampai jumpa lagi!")
 
